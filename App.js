b3e815f2aee71660dae9ee4f27cedc20d1b9947f
Mark <lisantimark@gmail.com>
2021-11-12 01:21:27 -0700

    websocket setup

--- App.js
+++ App.js
@@ -1,112 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// tesing websockets
 
 import React from 'react';
-import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Button,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// connecting to my desktop here
+const ws = new WebSocket('ws://10.0.0.4:8082');
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+ws.onopen = () => {
+  // open connection 
+  console.log('phone connected')
+};
+
+ws.onmessage = (e) => {
+  console.log(e.data);
+};
+
+ws.onerror = (e) => {
+  console.log(e.message);
+};
+
+ws.onclose = (e) => {
+  console.log(e.code,e.reason);
+}
+
+
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>HELLO HOWDY!</Text>
+      <View style={styles.space}/>
+      <Button
+        style={styles.button}
+        title="RED"
+        onPress={() => ws.send('red')}
+      />
+      <View style={styles.space}/>
+      <Button
+        style={styles.button}
+        title="BLUE"
+        onPress={() => ws.send('blue')}
+      />
+      <View style={styles.space}/>
+      <Button
+        style={styles.button}
+        title="YELLOW"
+        onPress={() => ws.send('yellow')}
+      />
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  button: {
+    marginBotton: 20,
+    padding: 30
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  space: {
+    width: 20,
+    height: 20
+  }
 });
 
 export default App;
